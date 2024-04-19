ddb115e4e7f7a627852e31d7406d80958da5d4c9
Syed Wali <syed.wali@ofqual.gov.uk>
2024-04-19 16:45:44 +0100

    feat: added pagination to organistions search results page

--- Models/OrganisationSearchResultViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ofqual.Common.RegisterFrontend.Models
-{
-    public class OrganisationSearchResultViewModel
-    {
-        public string? Name {  get; set; }
-        public required APIResponseList<OrganisationListViewModel> List { get; set; }
-    }
-}
---
+++ Models/Utilities.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace Ofqual.Common.RegisterFrontend.Models
+{
+    public static class Utilities
+    {
+        public static List<int> GeneratePageList(int currentPage, int itemsCount, int limit)
+        {
+            List<int> pageList = new List<int>();
+            var lastPage = itemsCount / limit;
+
+            //for the last page if needed
+            lastPage = itemsCount % limit > 0 ? lastPage + 1 : lastPage;
+
+            const int maxPagesToShow = 3; // Number of pages to show in the list (excluding ellipses)
+
+            int startPage = Math.Max(1, currentPage - (maxPagesToShow / 2));
+            int endPage = Math.Min(lastPage, startPage + maxPagesToShow - 1);
+
+            if (endPage - startPage + 1 < maxPagesToShow)
+            {
+                startPage = Math.Max(1, lastPage - maxPagesToShow + 1);
+            }
+
+            for (int i = startPage; i <= endPage; i++)
+            {
+                pageList.Add(i);
+            }
+
+            if (startPage > 1)
+            {
+                pageList.Insert(0, 1); 
+                if (startPage > 2)
+                {
+                    // Ellipses placeholder (-1 results in ... in the view)
+                    pageList.Insert(1, -1); 
+                }
+            }
+
+            if (endPage < lastPage)
+            {
+                if (endPage < lastPage - 1)
+                {
+                    pageList.Add(-1); 
+                }
+                pageList.Add(lastPage); 
+            }
+
+            return pageList;
+        }
+    }
+}
