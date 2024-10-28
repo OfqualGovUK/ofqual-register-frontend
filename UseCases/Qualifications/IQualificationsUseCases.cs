c4e0971c7132ad6d1df916e7cd07a9628ec50d52
PeterTully <34942822+PeterTully@users.noreply.github.com>
2024-10-28 15:13:50 +0000

    Update IQualificationsUseCases.cs
    roll back

--- UseCases/Qualifications/IQualificationsUseCases.cs
+++ UseCases/Qualifications/IQualificationsUseCases.cs
@@ -6,8 +6,6 @@ namespace Ofqual.Common.RegisterFrontend.UseCases.Qualifications
     public interface IQualificationsUseCases
     {
         Dictionary<string, string[]> CompareQuals(Qualification leftRef, Qualification rightRef);
-        string CreatePagedFilters(string? title, string? availability, string? qualificationTypes, string? qualificationLevels, string[] awardingOrganisations, string? sectorSubjectAreas, string? gradingTypes, string? assessmentMethods, string? nationalAvailability, int? minTotalQualificationTime, int? maxTotalQualificationTime, int? minGuidedLearninghours, int? maxGuidedLearninghours);
-
-        Task<QualificationFilterModel> GetFilterValues();
+        string CreatePagedFilters(string? title, string? availability, string? qualificationTypes, string? qualificationLevels, string? awardingOrganisations, string? sectorSubjectAreas, string? gradingTypes, string? assessmentMethods, string? nationalAvailability, int? minTotalQualificationTime, int? maxTotalQualificationTime, int? minGuidedLearninghours, int? maxGuidedLearninghours);
     }
 }
